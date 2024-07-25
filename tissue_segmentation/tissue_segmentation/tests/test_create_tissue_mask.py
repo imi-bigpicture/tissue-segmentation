37841e7ca892feeb7cbf19a871d77d58fde5e04f
budelmann <daniel.budelmann@mevis.fraunhofer.de>
2024-07-25 09:54:21 +0200

    Added test images and network weights

--- tissue_segmentation/tissue_segmentation/tests/test_create_tissue_mask.py
+++ tissue_segmentation/tissue_segmentation/tests/test_create_tissue_mask.py
@@ -10,11 +10,11 @@ def test_create_tissue_mask():
     # the test data does not have the correct resolution, which should ideally be around 8 microns
     # - network was trained using 8 microns resolution
     # - additionally the dilation of the mask has a default disk size of 32 pixels
-    #   (can be adhusted using dilation_disk_size parameter)
-    image = io.imread(os.path.join(os.path.dirname(__file__), "data", "0.jpg"))
+    #   (can be adjusted using dilation_disk_size parameter)
+    image = io.imread(os.path.join(os.path.dirname(__file__), "data", "29.jpg"))
     mask = create_tissue_mask(image)
     mask = 255 * mask.astype(np.uint8)
     mask_ref = io.imread(
-        os.path.join(os.path.dirname(__file__), "data", "0_mask_ref.png")
+        os.path.join(os.path.dirname(__file__), "data", "29_mask_ref.png")
     )
     assert np.sum(mask) == np.sum(mask_ref)
