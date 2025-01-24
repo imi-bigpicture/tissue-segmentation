08d0be20308ffd984355a40e333f43454524bc79
budelmann <daniel.budelmann@mevis.fraunhofer.de>
2025-01-24 11:06:01 +0100

    Added flake for nix development

---
+++ tissue_segmentation/flake.nix
@@ -0,0 +1,21 @@
+	{
+	description = "Developer shell for Tissue Segmentation";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+  };
+
+	outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { 
+        inherit system;
+      };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          poetry
+        ];
+      };
+    };
+}
